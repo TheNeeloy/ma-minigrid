6489a3d3e2b27ba1bc8178e36961a211cb9cfaef
TheNeeloy <neeloy1@gmail.com>
2020-09-10 15:25:30 -0500

    first revision of ma minigrid class

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='gym_minigrid',
     version='1.0.1',
     keywords='memory, environment, agent, rl, openaigym, openai-gym, gym',
-    url='https://github.com/maximecb/gym-minigrid',
+    url='https://github.com/TheNeeloy/ma-minigrid',
     description='Minimalistic gridworld package for OpenAI Gym',
     packages=['gym_minigrid', 'gym_minigrid.envs'],
     install_requires=[
